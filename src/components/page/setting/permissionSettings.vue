ffeeac97e3dd6c67220e3ac7f223ea535df59a06
Mr.Yuan <755966092@qq.com>
2017-12-01 18:01:42 +0800

    角色权限接口12-01

--- src/components/page/setting/permissionSettings.vue
+++ src/components/page/setting/permissionSettings.vue
@@ -9,16 +9,14 @@
                 <div class="block">
                     <el-cascader
                         expand-trigger="hover"
-                        :options="options"
+                        :options="parentCompanyList"
                         v-model="selectedOptions2"
                         @change="handleChange">
                     </el-cascader>
                 </div>
                 <div id="radioBtn">
                     <el-radio-group v-model="radio3">
-                        <el-radio-button class="radioBtn" label="超级管理员"></el-radio-button>
-                        <el-radio-button class="radioBtn" label="普通管理员"></el-radio-button>
-                        <el-radio-button class="radioBtn" label="普通用户"></el-radio-button>
+                        <el-radio-button class="radioBtn" v-for="item in allRoleList" :label="item.name"></el-radio-button>
                     </el-radio-group>
                 </div>
             </el-col>
@@ -303,202 +301,9 @@
 
                 },
                 activeName2: "first",
-                radio3: "超级管理员",
-                options: [{
-                    value: 'zhinan',
-                    label: '指南',
-                    children: [{
-                        value: 'shejiyuanze',
-                        label: '设计原则',
-                        children: [{
-                            value: 'yizhi',
-                            label: '一致'
-                        }, {
-                            value: 'fankui',
-                            label: '反馈'
-                        }, {
-                            value: 'xiaolv',
-                            label: '效率'
-                        }, {
-                            value: 'kekong',
-                            label: '可控'
-                        }]
-                    }, {
-                        value: 'daohang',
-                        label: '导航',
-                        children: [{
-                            value: 'cexiangdaohang',
-                            label: '侧向导航'
-                        }, {
-                            value: 'dingbudaohang',
-                            label: '顶部导航'
-                        }]
-                    }]
-                }, {
-                    value: 'zujian',
-                    label: '组件',
-                    children: [{
-                        value: 'basic',
-                        label: 'Basic',
-                        children: [{
-                            value: 'layout',
-                            label: 'Layout 布局'
-                        }, {
-                            value: 'color',
-                            label: 'Color 色彩'
-                        }, {
-                            value: 'typography',
-                            label: 'Typography 字体'
-                        }, {
-                            value: 'icon',
-                            label: 'Icon 图标'
-                        }, {
-                            value: 'button',
-                            label: 'Button 按钮'
-                        }]
-                    }, {
-                        value: 'form',
-                        label: 'Form',
-                        children: [{
-                            value: 'radio',
-                            label: 'Radio 单选框'
-                        }, {
-                            value: 'checkbox',
-                            label: 'Checkbox 多选框'
-                        }, {
-                            value: 'input',
-                            label: 'Input 输入框'
-                        }, {
-                            value: 'input-number',
-                            label: 'InputNumber 计数器'
-                        }, {
-                            value: 'select',
-                            label: 'Select 选择器'
-                        }, {
-                            value: 'cascader',
-                            label: 'Cascader 级联选择器'
-                        }, {
-                            value: 'switch',
-                            label: 'Switch 开关'
-                        }, {
-                            value: 'slider',
-                            label: 'Slider 滑块'
-                        }, {
-                            value: 'time-picker',
-                            label: 'TimePicker 时间选择器'
-                        }, {
-                            value: 'date-picker',
-                            label: 'DatePicker 日期选择器'
-                        }, {
-                            value: 'datetime-picker',
-                            label: 'DateTimePicker 日期时间选择器'
-                        }, {
-                            value: 'upload',
-                            label: 'Upload 上传'
-                        }, {
-                            value: 'rate',
-                            label: 'Rate 评分'
-                        }, {
-                            value: 'form',
-                            label: 'Form 表单'
-                        }]
-                    }, {
-                        value: 'data',
-                        label: 'Data',
-                        children: [{
-                            value: 'table',
-                            label: 'Table 表格'
-                        }, {
-                            value: 'tag',
-                            label: 'Tag 标签'
-                        }, {
-                            value: 'progress',
-                            label: 'Progress 进度条'
-                        }, {
-                            value: 'tree',
-                            label: 'Tree 树形控件'
-                        }, {
-                            value: 'pagination',
-                            label: 'Pagination 分页'
-                        }, {
-                            value: 'badge',
-                            label: 'Badge 标记'
-                        }]
-                    }, {
-                        value: 'notice',
-                        label: 'Notice',
-                        children: [{
-                            value: 'alert',
-                            label: 'Alert 警告'
-                        }, {
-                            value: 'loading',
-                            label: 'Loading 加载'
-                        }, {
-                            value: 'message',
-                            label: 'Message 消息提示'
-                        }, {
-                            value: 'message-box',
-                            label: 'MessageBox 弹框'
-                        }, {
-                            value: 'notification',
-                            label: 'Notification 通知'
-                        }]
-                    }, {
-                        value: 'navigation',
-                        label: 'Navigation',
-                        children: [{
-                            value: 'menu',
-                            label: 'NavMenu 导航菜单'
-                        }, {
-                            value: 'tabs',
-                            label: 'Tabs 标签页'
-                        }, {
-                            value: 'breadcrumb',
-                            label: 'Breadcrumb 面包屑'
-                        }, {
-                            value: 'dropdown',
-                            label: 'Dropdown 下拉菜单'
-                        }, {
-                            value: 'steps',
-                            label: 'Steps 步骤条'
-                        }]
-                    }, {
-                        value: 'others',
-                        label: 'Others',
-                        children: [{
-                            value: 'dialog',
-                            label: 'Dialog 对话框'
-                        }, {
-                            value: 'tooltip',
-                            label: 'Tooltip 文字提示'
-                        }, {
-                            value: 'popover',
-                            label: 'Popover 弹出框'
-                        }, {
-                            value: 'card',
-                            label: 'Card 卡片'
-                        }, {
-                            value: 'carousel',
-                            label: 'Carousel 走马灯'
-                        }, {
-                            value: 'collapse',
-                            label: 'Collapse 折叠面板'
-                        }]
-                    }]
-                }, {
-                    value: 'ziyuan',
-                    label: '资源',
-                    children: [{
-                        value: 'axure',
-                        label: 'Axure Components'
-                    }, {
-                        value: 'sketch',
-                        label: 'Sketch Templates'
-                    }, {
-                        value: 'jiaohu',
-                        label: '组件交互文档'
-                    }]
-                }],
+                radio3: '',
+                parentCompanyList: [],
+                allRoleList: [],
                 selectedOptions: [],
                 selectedOptions2: []
             };
@@ -513,9 +318,49 @@
             checkChange(event) {
                 //   console.log(event);
                 console.log(this.checkBoxData);
-
-
-            }
+            },
+            // 所有子公司
+            applyCompany() {
+                let self = this;
+                this.$axios({
+                    method: 'POST',
+                    withCredentials: false,
+                    url: '/api/company/CompanyMyList',
+                    data: {
+                        token: "1511328705UZVQ",
+                    }
+                })
+                .then(function (res) {
+                    self.parentCompanyList = res.data.data.list
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+            },
+            // 所有角色
+            getAllRole() {
+                let self = this;
+                this.$axios({
+                    method: 'POST',
+                    withCredentials: false,
+                    url: '/api/Role/roleList',
+                    data: {
+                        token: "1511328705UZVQ",
+                    }
+                })
+                    .then(function (res) {
+                        self.allRoleList = res.data.data.list
+                        self.radio3 = res.data.data.list[0].name
+                        self.dataPermission = res.data.data.list[0].data_auth
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            },
+        },
+        created() {
+            this.applyCompany();
+            this.getAllRole();
         }
     };
 </script>
@@ -536,9 +381,6 @@
         padding: 0 5px;
     }
 
-    .block {
-        padding-right: 15px;
-    }
 
     .checkTitle {
         margin: 10px 0;
@@ -556,6 +398,9 @@
     .el-radio {
         width: 100%;
         margin: 10px 0;
+    }
+    .el-radio-group {
+        width: 80%;
     }
 
     .el-radio + .el-radio {
